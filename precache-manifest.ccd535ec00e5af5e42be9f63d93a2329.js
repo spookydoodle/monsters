4756804205fac820a679ee07cda082402dfdf690
kxkaro <kar.krajewska@gmail.com>
2020-06-13 20:50:37 +0200

    Updates

---
+++ precache-manifest.ccd535ec00e5af5e42be9f63d93a2329.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4fc52fe16cd4b381320004a943f76dc",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "536b8bbc3badf1a0c3af",
+    "url": "/monsters/static/css/main.ace42ed4.chunk.css"
+  },
+  {
+    "revision": "6c1628f415af83ea4cb8",
+    "url": "/monsters/static/js/2.faa01a82.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/monsters/static/js/2.faa01a82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "536b8bbc3badf1a0c3af",
+    "url": "/monsters/static/js/main.8c4f9e73.chunk.js"
+  },
+  {
+    "revision": "360bbdfb15409043b41d",
+    "url": "/monsters/static/js/runtime-main.6c4a148e.js"
+  },
+  {
+    "revision": "3686de9116982afdc403fba951b3838d",
+    "url": "/monsters/static/media/Landing_1.3686de91.png"
+  },
+  {
+    "revision": "f19b7bf727f425faf68de11edd82068d",
+    "url": "/monsters/static/media/Landing_2.f19b7bf7.png"
+  },
+  {
+    "revision": "bf9e3a6dbee0072d04e8749fa8594337",
+    "url": "/monsters/static/media/Landing_3.bf9e3a6d.png"
+  },
+  {
+    "revision": "8e813d09e716433fb9149bd08cf76729",
+    "url": "/monsters/static/media/Landing_4.8e813d09.png"
+  },
+  {
+    "revision": "594422e35bfa860cecbd5c94b09ed94e",
+    "url": "/monsters/static/media/Landing_5.594422e3.png"
+  }
+]);
