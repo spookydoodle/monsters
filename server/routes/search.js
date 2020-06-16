c6ba678da68d49b758fc01046f14ac904aa840ed
kxkaro <kar.krajewska@gmail.com>
2020-06-16 13:44:42 +0200

    Add general route method

--- server/routes/search.js
+++ server/routes/search.js
@@ -7,38 +7,29 @@ const app = app => {
     // Catches requests made to localhost:4000/
     app.get('/', (req, res) => res.send('Hello Monster!'));
 
-
     // Web scraping with puppeteer library
-    app.get('/puppeteer/search', (req, res) => {
-        const query = req.query.q;
-        if (!query) res.status(400).send("Provide a valid query value using the parameter 'q'.")
-
-        searchPuppetier(query)
-            .then(results => res.status(200).send(results))
-            .catch(err => res.status(500).send({ err: "Error retrieving data", message: err }));
-    });
-
+    get(app, '/puppeteer/search', searchPuppetier)
 
     // Get results from the official google API. Free version includes only 10 results.
-    app.get('/google-api/search', (req, res) => {
-        const query = req.query.q;
-        if (!query) res.status(400).send("Provide a valid query value using the parameter 'q'.")
-
-        searchGoogleAPI(query)
-            .then(results => res.status(200).send(results))
-            .catch(err => res.status(500).send({ err: "Error retrieving data", message: err }));
-    });
-
+    get(app, '/google-api/search', searchGoogleAPI)
 
     // Get data from raw HTML / web scraping
-    app.get('/html/search', (req, res) => {
+    get(app, '/html/search', searchHTML)
+
+
+}
+
+// General method for getting data using selected method
+const get = (app, path, method) => {
+    app.get(path, (req, res) => {
         const query = req.query.q;
         if (!query) res.status(400).send("Provide a valid query value using the parameter 'q'.")
 
-        searchHTML(query)
+        method(query)
             .then(results => res.status(200).send(results))
             .catch(err => res.status(500).send({ err: "Error retrieving data", message: err }));
     });
 }
+
 
 module.exports = app;
