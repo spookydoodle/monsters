d4573eba2e064ae68b1b17dfea4ac6cd2096bd0e
kxkaro <kar.krajewska@gmail.com>
2020-06-28 20:56:37 +0200

    add deploy config

---
+++ client/build/precache-manifest.376d30ff254b531f72dbe00ff5232c14.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4424c3107101f2f7c17331cb2bd5118",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "a9d2d49b2f344179606b",
+    "url": "/monsters/static/css/main.6a3f9d67.chunk.css"
+  },
+  {
+    "revision": "d8b2d1ee9f3f79939b32",
+    "url": "/monsters/static/js/2.c24454a1.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/monsters/static/js/2.c24454a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9d2d49b2f344179606b",
+    "url": "/monsters/static/js/main.2fa1a14e.chunk.js"
+  },
+  {
+    "revision": "3dab8aa598718cb8e8b3",
+    "url": "/monsters/static/js/runtime-main.f48c3991.js"
+  },
+  {
+    "revision": "026a1340abfe46f6ce57a3533faa7ab8",
+    "url": "/monsters/static/media/JumbotronDark.026a1340.png"
+  },
+  {
+    "revision": "49496ca5d835f8c7a55f97d3544ed0e5",
+    "url": "/monsters/static/media/JumbotronLight.49496ca5.png"
+  },
+  {
+    "revision": "3686de9116982afdc403fba951b3838d",
+    "url": "/monsters/static/media/Landing_1.3686de91.png"
+  },
+  {
+    "revision": "f19b7bf727f425faf68de11edd82068d",
+    "url": "/monsters/static/media/Landing_2.f19b7bf7.png"
+  },
+  {
+    "revision": "bf9e3a6dbee0072d04e8749fa8594337",
+    "url": "/monsters/static/media/Landing_3.bf9e3a6d.png"
+  },
+  {
+    "revision": "8e813d09e716433fb9149bd08cf76729",
+    "url": "/monsters/static/media/Landing_4.8e813d09.png"
+  },
+  {
+    "revision": "594422e35bfa860cecbd5c94b09ed94e",
+    "url": "/monsters/static/media/Landing_5.594422e3.png"
+  },
+  {
+    "revision": "f064e9ed54cb7c98804fffd1d4146079",
+    "url": "/monsters/static/media/Register.f064e9ed.png"
+  }
+]);
