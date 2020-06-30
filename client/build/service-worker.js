7787dbbcc351a3077329b4eb62077dcff97bdcac
kxkaro <kar.krajewska@gmail.com>
2020-06-30 22:08:30 +0200

    add app.yaml files for google cloud deployment

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.bbade4028cb75f2a880be74142510bc0.js"
+  "/precache-manifest.fa467a01e328c6da959fec6f5839f467.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
     '/api',
     createProxyMiddleware({
     //   target: process.env.REACT_APP_PROXY || 'http://localhost:4000',
-      target: process.env.NODE_ENV === 'production' ? 'http://api:4000' : 'http://localhost:4000',
+      target: process.env.NODE_ENV === 'production' ? 'https://api-dot-monsters-281717.ew.r.appspot.com:4000' : 'http://localhost:4000',
       changeOrigin: true,
     })
   );
