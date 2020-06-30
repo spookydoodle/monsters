41b882ee4bc4b85409c0507c4777652491d9f0f9
kxkaro <kar.krajewska@gmail.com>
2020-06-30 12:44:29 +0200

    add clock class component

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,8 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: process.env.REACT_APP_PROXY || 'http://localhost:4000',
+    //   target: process.env.REACT_APP_PROXY || 'http://localhost:4000',
+      target: process.env.NODE_ENV === 'production' ? 'http://api:4000' : 'http://localhost:4000',
       changeOrigin: true,
     })
   );
